94733140e633789c60e21572fe919bd7044a90ea
jasmineranay <102012764+jasmineranay@users.noreply.github.com>
2022-05-19 22:55:49 -0700

    add

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 // TODO: Include packages needed for this application
-
+const inq = require('inquirer');
+const chalk = require('chalk');
+const fs = require('fs');
+// const { create } = require('domain');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
 
-// TODO: Create a function to initialize app
-function init() {}
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
 
-// Function call to initialize app
-init();
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+]
+
+async function createReadme(answers) {
+ fs.writeFileSync('./READ.md', `
+ # ${answers.title}
+ ## Description
+ ${answers.description}
+ ## Installation Instructions
+ ${answers.installation}
+ ## Deployed Link
+ [deployed link]($answers{answers.deployedLink})
+ `)
+}
+
+inq
+.prompt(questions)
+.then((answers) => {
+    createReadme(answers)
+    console.log(chalk.green('Sucessfully created README.md'))
+})
+.catch((error) => {
+    if (error.isTtyError) {
+        console.error('Prompts could not be rendered in current environment!')
+    } else {
+        console.error(`Something went wrong!`, error)
+    }
+    
+});
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
+
+createReadme()
