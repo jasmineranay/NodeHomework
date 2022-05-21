d4d47e55fdacd4a32d41f5319765087dba9ea5aa
jasmineranay <102012764+jasmineranay@users.noreply.github.com>
2022-05-21 10:23:51 -0700

    add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inq = require('inquirer');
 const chalk = require('chalk');
 const fs = require('fs');
+const { windowCount } = require ('rx.js');
 // const { create } = require('domain');
 // TODO: Create an array of questions for user input
 const questions = [
@@ -22,6 +23,25 @@ const questions = [
         name: 'installation',
         message: 'How do you install your project?',
     },
+
+    {
+		type: 'input',
+		message: 'What is your application used for?',
+		name: 'usage',
+		// default: 'Gaining EXP with Node',
+	},
+    {
+		type: 'checkbox',
+		message: 'What kind of license is used?',
+		name: 'license',
+		choices: ['BSD', 'MIT', 'GPL'],
+	},
+    {
+		type: 'input',
+		message: 'What are the guidelines to contributing?',
+		name: 'contribute',
+		// default: 'You can NOT',
+	},
     {
 
         type: 'input',
@@ -42,44 +62,78 @@ const questions = [
     },
 ]
 
-function createReadme(answers) {
- fs.writeFileSync('./READ.md', `
- # ${answers.title}
- ## Description
- ${answers.description}
- ## Installation Instructions
- ${answers.installation}
- ## Deployed Link
- [deployed link]($answers{answers.deployedLink})
- ${answers.githubusername}
- ## GitHub Username
- ${answers.githubusername}
- ## Email
- [deployed link]($answers{answers.email})
- `)
-}
-
-inq
-.prompt(questions)
-.then((answers) => {
-    createReadme(answers)
-    console.log(chalk.green('Sucessfully created README.md'))
-})
-.catch((error) => {
-    if (error.isTtyError) {
-        console.error('Prompts could not be rendered in current environment!')
-    } else {
-        console.error(`Something went wrong!`, error)
-    }
+function createReadme(data) {
+    var license = '';
+    var licensetext = '';
+    switch (data.license[0]) {
+        switch (data.license[0]) {
+            case 'BSD':
+                license =
+                    '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+                licenseTXT = `[BSD License](https://opensource.org/licenses/BSD-3-Clause)`;
+                break;
+            case 'MIT':
+                license =
+                    '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+                licenseTXT = `[MIT License](https://opensource.org/licenses/MIT)`;
+                break;
+            case 'GPL':
+                license =
+                    '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+                licenseTXT = `[GPL License](https://www.gnu.org/licenses/gpl-3.0)`;
     
-});
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
-
-createReadme()
+                break;
+            default:
+                license = 'No License Chosen';
+                licenseTXT = ``;
+    
+                break;
+        }
+        fs.writeFileSync(
+            './readme.md',
+            `# ${data.project}
+    ${license}
+    ## Table of Contents
+    - [Description](#description)
+    - [Application Preview](#application-preview)
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributing](#contributing)
+    - [Test Instructions](#test-instructions)
+    - [Questions](#questions)
+    - [License](#license)
+    ## Description
+    - ${data.description}
+    ## Application Preview
+    <p align="left">
+    <img alt="README Gen Demo 1" src="${data.preview}">
+    </p>
+    ## Installation
+    - ${data.install}
+    ## Usage
+    - ${data.usage}
+    ## Contributing
+    - ${data.contribute}
+    ## Test Instructions
+    - ${data.test}
+    ## Questions
+    Questions? Concerns?  Contact Me Below:
+    - Github Username: ${data.github}
+    - Github Link: https://github.com/${data.github} 
+    - Email: ${data.email}
+    ## License
+    - Copyright 2022 Brian Alegre
+    - Licensed under the: ${licenseTXT} 
+    `
+        );
+    };
+    
+    // CLI Prompts
+    inquirer.prompt(questions).then((data) => {
+        createReadme(data);
+        const filename = `${data.project.toLowerCase().split(' ').join('')}.json`;
+    
+        fs.writeFileSync(filename, JSON.stringify(data, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+        );
+    });
